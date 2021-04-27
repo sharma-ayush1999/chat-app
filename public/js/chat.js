2223e736b1fc7cfbbeaac27f77dbec00db5cf347
Ayush Sharma <ayush@theflak.in>
2021-04-27 16:05:39 +0530

    FOrm manipualtion added

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,16 +3,29 @@ const socket = io();
 // server (emit) -> client (recieve) --acknowledgement--> server
 // client (emit) -> server (recieve) --acknowledgement--> client
 
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  //disable
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
 
   socket.emit("sendMessage", message, (error) => {
+      //enable
+      $messageFormButton.removeAttribute('disabled');
+      $messageFormInput.value = '';
+      $messageFormInput.focus()
     if (error) {
       return console.log(error);
     }
@@ -20,11 +33,13 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
+  //disabled
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
 
+  $sendLocationButton.setAttribute("disabled", "disabled");
   navigator.geolocation.getCurrentPosition((position) => {
     // console.log(position.coords);
     socket.emit(
@@ -34,6 +49,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationButton.removeAttribute('disabled')
         console.log("Location shared");
       }
     );
