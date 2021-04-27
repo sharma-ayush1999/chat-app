68d2e7f846d29352bb6accd546f27935cd452372
Ayush Sharma <ayush@theflak.in>
2021-04-27 15:34:36 +0530

    oogle map integrated for sharing client location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,30 @@
 const socket = io();
 
-socket.on('message', (message) => {
-    console.log(message)
-})
+socket.on("message", (message) => {
+  console.log(message);
+});
 
-document.querySelector("#message-form").addEventListener('submit', (e) => {
-    e.preventDefault();
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const message = e.target.elements.message.value;
+  const message = e.target.elements.message.value;
 
-    socket.emit('sendMessage', message);
-})
+  socket.emit("sendMessage", message);
+});
 
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
 
+  navigator.geolocation.getCurrentPosition((position) => {
+    // console.log(position.coords);
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
 
 // socket.on('countUpdated', (count) => {
 //     console.log('The count has been updated!', count)
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
@@ -17,24 +17,39 @@ let count = 0;
 //  server (emit) -> client (recieve) - countUpdated
 // client (emit) -> server (recieve) - increment
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  console.log("New web socket connection");
 
-    console.log("New web socket connection");
+  socket.emit("message", "Welcome");
 
-    socket.emit('message', 'Welcome');
+  socket.broadcast.emit("message", "A new user has joined!");
 
-    socket.on('sendMessage', (message) => {
-        io.emit('message', message);
-    })
-    // socket.emit('countUpdated', count)
-    // socket.on('increment', () => {
-    //     count++;
-    //     //socket.emit('countUpdated', count)
-    //     io.emit('countUpdated', count)
-    // })
-})
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
 
-server.listen(port, () => {
-    console.log(`Server is up on port ${port}!`)
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
+  });
+
+  // socket.on('sendLocation', (coords) => {
+  //     io.emit('message', `Location:${coords.latitude}, ${coords.longitude}` )
+  // })
+
+  socket.on("sendLocation", (coords) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+  });
+  // socket.emit('countUpdated', count)
+  // socket.on('increment', () => {
+  //     count++;
+  //     //socket.emit('countUpdated', count)
+  //     io.emit('countUpdated', count)
+  // })
 });
 
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
