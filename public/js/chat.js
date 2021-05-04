a9626f5ea80f845395191e45c6c1ff5026bcc61a
Ayush Sharma <ayush@theflak.in>
2021-05-04 11:59:47 +0530

    Uto scroll functionality added

--- public/js/chat.js
+++ public/js/chat.js
@@ -23,6 +23,31 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
+const autoScroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild;
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight;
+
+    // How far have I scrolled"
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+      $messages.scrollTop = $messages.scrollHeight;
+      console.log($messages.scrollTop);
+    }
+
+
+}
+
 socket.on("message", (message) => {
   const html = Mustache.render(messageTemplate, {
     username: message.username,
@@ -30,6 +55,7 @@ socket.on("message", (message) => {
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll()
 });
 
 socket.on("locationMessage", (message) => {
@@ -39,6 +65,7 @@ socket.on("locationMessage", (message) => {
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll()
 });
 
 socket.on('roomData', ({room, users}) => {
@@ -47,6 +74,7 @@ socket.on('roomData', ({room, users}) => {
     users
   });
   document.querySelector("#sidebar").innerHTML = html;
+  // autoScroll();
 })
 $messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
